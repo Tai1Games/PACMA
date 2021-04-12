f7cf14b380f88529785379fce6557a57a68a2261
Miguel Mur Cortés <mmur@ucm.es>
2021-04-12 20:21:41 +0200

    Completada la creacion de los cartelitos

--- PACMA/Assets/Scripts/Poste.cs
+++ PACMA/Assets/Scripts/Poste.cs
@@ -3,16 +3,70 @@ using System.Collections.Generic;
 using UnityEngine;
 using Utility;
 
+/*      Ejemplo para crear postes. Se que se podria hacer mejor lo de los vectores pero estamos aqui para mermedalear.
+ 
+        //POSTE DE TAMAÑO 1
+
+        Poste poste = Instantiate(vecPostes[0]).GetComponent<Poste>();
+        List<Direccion> direcciones = new List<Direccion>();
+
+        Direccion dir;
+        dir.sentido = Sentido.Derecha;
+        dir.destino = 0;
+
+        direcciones.Add(dir);
+
+        poste.Init(new Vector3(0, 0, 10), direcciones);
+
+        //POSTE DE TAMAÑO 2
+        poste = Instantiate(vecPostes[1]).GetComponent<Poste>();
+        direcciones = new List<Direccion>();
+
+        dir.sentido = Sentido.Derecha;
+        dir.destino = 0;
+
+        direcciones.Add(dir);
+
+        dir.sentido = Sentido.Izquierda;
+        dir.destino = 0;
+
+        direcciones.Add(dir);
+
+        poste.Init(new Vector3(0, 0, 20), direcciones);
+
+        //POSTE DE TAMAÑO 3
+        poste = Instantiate(vecPostes[2]).GetComponent<Poste>();
+        direcciones = new List<Direccion>();
+
+        dir.sentido = Sentido.Derecha;
+        dir.destino = 0;
+
+        direcciones.Add(dir);
+
+        dir.sentido = Sentido.Derecha;
+        dir.destino = 0;
+
+        direcciones.Add(dir);
+
+        dir.sentido = Sentido.Derecha;
+        dir.destino = 0;
+
+        direcciones.Add(dir);
+
+        poste.Init(new Vector3(0, 0, 30), direcciones);
+*/
+
+
 public class Poste : MonoBehaviour
 {
-    public GameObject padreCarteles;
+    public Transform padreCarteles;
     int nCarteles;
-    GameObject[] carteles;
+    List<GameObject> carteles = new List<GameObject>();
+
     // Start is called before the first frame update
     void Start()
     {
-        carteles = padreCarteles.GetComponentsInChildren<GameObject>();
-        nCarteles = carteles.Length;
+
     }
 
     // Update is called once per frame
@@ -23,20 +77,26 @@ public class Poste : MonoBehaviour
 
     public void Init(Vector3 pos, List<Utility.Direccion> direcciones)
     {
+        foreach (Transform hijo in padreCarteles)
+        {
+            carteles.Add(hijo.gameObject);
+        }
+        nCarteles = carteles.Count;
+
         transform.position = pos;
         for (int k = 0; k < direcciones.Count; k++)
         {
             //Giramos los carteles
-            if (direcciones[k].sentido == Sentido.Derecha) carteles[k].transform.rotation = new Quaternion (carteles[k].transform.rotation.x, 0, 180, 0);
+            if (direcciones[k].sentido == Sentido.Derecha) carteles[k].transform.localScale = new Vector3(-carteles[k].transform.lossyScale.x, carteles[k].transform.lossyScale.y, carteles[k].transform.lossyScale.z);
 
             //Aquí poner las imágenes pero por el momento xd
 
             //Personalizar un poco los carteles
-            float scaleVar = Random.Range(1.0f, 2.0f);
+            float scaleVar = Random.Range(1.0f, 1.2f);
             carteles[k].transform.localScale *= scaleVar;
 
-            float rotVar = Random.Range(-15, 15);
-
+            float rotVar = Random.Range(-20, 20);
+            carteles[k].transform.Rotate(new Vector3(0, 1, 0), rotVar);
         }
     }
 }
