7ce86ef797e02d4f3f505e672af279331192c930
Esteban <35503563+EstebenR@users.noreply.github.com>
2021-04-13 13:18:24 +0200

    Menu principal (#8)
    * Empezado menú
    
    * Gamemanager creado y añadidos prefabs
    
    * Menu principal provisional
    
    * Más bonito

---
+++ PACMA/Assets/Scripts/GameManager.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager _instance;
+
+    public static GameManager instance = null;
+
+    public string gameSceneName;
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+            Destroy(this.gameObject);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SendCommand(string command)
+    {
+        if (command == "Derecha")
+        {
+            Debug.Log("Girando a derecha");
+        }else if(command == "Izquierda")
+        {
+            Debug.Log("Girando a izquierda");
+        }
+        else if (command == "Start")
+        {
+            Debug.Log("Vamo a jugal");
+            StartCoroutine(LoadSceneAsync("t_Postes"));
+        }else if(command == "Exit"){
+            Debug.Log("Saliendo del juego");
+            Application.Quit();
+        }
+    }
+
+    IEnumerator LoadSceneAsync(string sceneName)
+    {
+        Debug.Log("Loading scene: " + sceneName);
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(sceneName);
+
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+    }
+}
