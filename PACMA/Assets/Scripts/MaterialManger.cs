bd6d36e91e70359cc913ed658612afc8d6d3a9ea
Andrés <47799692+AndresRuizB@users.noreply.github.com>
2021-04-13 14:44:13 +0200

    variosmateriales (#9)
    * Colorines que cambian
    
    * Terminadas paletas
    
    * Añadidos materiales al menu principal
    
    colores bonitas uwu
    
    * mas colores

---
+++ PACMA/Assets/Scripts/MaterialManger.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaterialManger : MonoBehaviour
+{
+	public Material[] arrayMateriales;
+
+	public TextAsset archivoPaletas;
+
+	private Color[][] Paletas;
+
+	public int paletaActual = 0;
+
+	int nPaletas;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		Resources.FindObjectsOfTypeAll(typeof(Material));
+
+		string[] lineasArchivo = archivoPaletas.text.Split('\n');
+
+		nPaletas = int.Parse(lineasArchivo[0]);
+		int nColores = int.Parse(lineasArchivo[1]);
+		Paletas = new Color[nPaletas][];
+
+		for (int i = 0; i < nPaletas; i++)
+		{
+			Paletas[i] = new Color[nColores];
+
+			for (int j = 0; j < nColores; j++)
+			{
+				Paletas[i][j] = vectorFromString(lineasArchivo[2 + i * (nColores+1) + j + 1].Substring(5));
+			}
+		}
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		for (int i = 0; i < arrayMateriales.Length; i++)
+		{
+			arrayMateriales[i].color = Paletas[paletaActual%nPaletas][i];
+		}
+	}
+
+	public Color vectorFromString(string s)
+	{
+
+		string[] sF = s.Split(',');
+
+		Vector4 ret = new Vector4(float.Parse(sF[0]) / 255, float.Parse(sF[1]) / 255, float.Parse(sF[2]) / 255, 1.0f);
+
+		return ret;
+	}
+}
