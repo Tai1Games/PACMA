8adee2bc1a2a54472216159db7f4289f617947cb
AdriAlvarez00 <adralv01@ucm.es>
2021-04-13 17:29:41 +0200

    Generacion basica de ciudad

--- PACMA/Assets/Scripts/utility.cs
+++ PACMA/Assets/Scripts/utility.cs
@@ -1,9 +1,17 @@
+
 namespace Utility
 {
     public enum Sentido
     {
         Derecha = 0,
-        Izquierda = 1
+        Izquierda = 1,
+        Recto = 2 // Espero no romper nada juejuejue
+    }
+    public enum Cardinal { 
+        North = 0,
+        West = 1,
+        South = 2,
+        East = 3
     }
 
     public struct Direccion
