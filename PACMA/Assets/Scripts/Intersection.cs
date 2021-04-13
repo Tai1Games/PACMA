17e7651f12245b42f1a40b98a4430d262f32c71e
Esteban <35503563+EstebenR@users.noreply.github.com>
2021-04-13 23:14:26 +0200

    Generacion de mapa (#15)
    * Generacion basica de ciudad
    
    * Partidamanager empezada
    
    * La escena xd
    
    * Assets y empezamos el A R T E
    
    * Metidos 3 prefabs nuevos
    
    * 2 Intersecciones +
    
    * Unificados prefabs y fin de las intersecciones prohibidas
    
    * Colisiones
    
    * Ya se genera ciudadela
    
    * Ahora va desde menu principal
    
    * Actualizado nombre de escena
    
    Co-authored-by: AdriAlvarez00 <adralv01@ucm.es>
    Co-authored-by: Miguel Mur Cortés <mmur@ucm.es>

---
+++ PACMA/Assets/Scripts/Intersection.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Intersection : MonoBehaviour
+{
+    public List<Utility.Sentido> salidas;
+}
--- PACMA/Assets/Scripts/utility.cs
+++ PACMA/Assets/Scripts/utility.cs
@@ -1,15 +1,22 @@
+
 namespace Utility
 {
     public enum Sentido
     {
         Derecha = 0,
         Izquierda = 1,
-        Recto
+        Recto = 2 // Espero no romper nada juejuejue
+    }
+    public enum Cardinal { 
+        North = 0,
+        West = 1,
+        South = 2,
+        East = 3
     }
 
     public struct Direccion
     {
         public Sentido sentido;
-        public int destino; //Esto se cambiará? Posiblemente
+        public int destino; //Esto se cambiarï¿½? Posiblemente
     }
 }
