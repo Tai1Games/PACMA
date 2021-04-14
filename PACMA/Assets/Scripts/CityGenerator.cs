15db22bdc52e4e214adb0cce1d2ea68a1e6bd095
Andres <47799692+AndresRuizB@users.noreply.github.com>
2021-04-14 21:25:42 +0200

    ya gira

--- PACMA/Assets/Scripts/CityGenerator.cs
+++ PACMA/Assets/Scripts/CityGenerator.cs
@@ -5,125 +5,153 @@ using Utility;
 
 public class CityGenerator : MonoBehaviour
 {
-    //Parametros de generacion
-    public List<GameObject> straights;
-    public List<GameObject> intersections;
-    public int tileSize = 5;
-    public int maxStraight = 3;
-    public int minStraight = 1;
+	//Parametros de generacion
+	public List<GameObject> straights;
+	public List<GameObject> intersections;
+	public int tileSize = 5;
+	public int maxStraight = 3;
+	public int minStraight = 1;
 
-    //Jugador
-    public GameObject player;
-    public float playerSpeed = 1;
-    private bool moving = false;
-    private Sentido playerNextDir = Sentido.Recto;
+	public ParticleSystem ps;
 
-    //Listas para guardar las carreteras que vamos generando
-    private List<GameObject> currentCarretera;
-    GameObject inters = null, lastTile = null, tileOpt1 = null, tileOpt2 = null, oldInters = null;
-    Vector3 facingVec = new Vector3(0, 0, 1);
-    Vector3 origin = Vector3.zero;
+	//Jugador
+	public GameObject player;
+	public PlayerCollisionHandler playerColHandler;
+	public float playerSpeed = 1;
+	private bool moving = false;
+	private Sentido playerNextDir = Sentido.Recto;
 
-    GameObject PlaceTile(GameObject tile, Vector3 direccionVec, Vector3 previousTilePos)
-    {
-        return Instantiate(tile, previousTilePos + tileSize * direccionVec + origin, Quaternion.LookRotation(direccionVec, Vector3.up));
-    }
+	private bool tweetDer = false;
+	private bool tweetIz = false;
 
-    Vector3 rotaVector(Vector3 vec, Sentido dir)
-    {
-        if (dir == Sentido.Derecha)
-            return Quaternion.AngleAxis(90, Vector3.up) * vec;
-        else if (dir == Sentido.Izquierda)
-            return Quaternion.AngleAxis(-90, Vector3.up) * vec;
-        else
-            return vec;
-    }
+	//Listas para guardar las carreteras que vamos generando
+	private List<GameObject> currentCarretera;
+	GameObject inters = null, lastTile = null, tileOpt1 = null, tileOpt2 = null, oldInters = null;
+	Vector3 facingVec = new Vector3(0, 0, 1);
+	Vector3 origin = Vector3.zero;
 
-    void GeneraTramo(Vector3 direccionVec, Vector3 startPos)
-    {
-        lastTile = PlaceTile(straights[Random.Range(0, straights.Count)], direccionVec, startPos);
-        currentCarretera.Add(lastTile);
-        //currentCarretera.Add(PlaceTile(straights[Random.Range(0, straights.Count)], direccionVec, startPos));
-        //Coloca una cantidad de rectas aleatorias
-        int nTiles = Random.Range(minStraight, maxStraight);
-        for (int i = 1; i < nTiles; i++)
-        {
-            lastTile = PlaceTile(straights[Random.Range(0, straights.Count)], direccionVec, lastTile.transform.position);
-            //currentCarretera.Add(PlaceTile(straights[Random.Range(0, straights.Count)], direccionVec, currentCarretera[i-1].transform.position));
-            currentCarretera.Add(lastTile);
-        }
-        //Coloca una interseccion
-        inters = PlaceTile(intersections[Random.Range(0, intersections.Count)], direccionVec, lastTile.transform.position);
-        //Generar una tile mas a cada lado de la interseccion
-        tileOpt1 = PlaceTile(straights[Random.Range(0, straights.Count)], rotaVector(direccionVec, Sentido.Izquierda), inters.transform.position);
+	GameObject PlaceTile(GameObject tile, Vector3 direccionVec, Vector3 previousTilePos)
+	{
+		return Instantiate(tile, previousTilePos + tileSize * direccionVec + origin, Quaternion.LookRotation(direccionVec, Vector3.up));
+	}
 
-        tileOpt2 = PlaceTile(straights[Random.Range(0, straights.Count)], rotaVector(direccionVec, Sentido.Derecha), inters.transform.position);
-    }
+	Vector3 rotaVector(Vector3 vec, Sentido dir)
+	{
+		if (dir == Sentido.Derecha)
+			return Quaternion.AngleAxis(90, Vector3.up) * vec;
+		else if (dir == Sentido.Izquierda)
+			return Quaternion.AngleAxis(-90, Vector3.up) * vec;
+		else
+			return vec;
+	}
 
-    void cleanCarretera()
-    {
-        foreach(GameObject item in currentCarretera)
-        {
-            Destroy(item);
-        }
-        currentCarretera.Clear();
-    }
+	void GeneraTramo(Vector3 direccionVec, Vector3 startPos)
+	{
+		lastTile = PlaceTile(straights[Random.Range(0, straights.Count)], direccionVec, startPos);
+		currentCarretera.Add(lastTile);
+		//currentCarretera.Add(PlaceTile(straights[Random.Range(0, straights.Count)], direccionVec, startPos));
+		//Coloca una cantidad de rectas aleatorias
+		int nTiles = Random.Range(minStraight, maxStraight);
+		for (int i = 1; i < nTiles; i++)
+		{
+			lastTile = PlaceTile(straights[Random.Range(0, straights.Count)], direccionVec, lastTile.transform.position);
+			//currentCarretera.Add(PlaceTile(straights[Random.Range(0, straights.Count)], direccionVec, currentCarretera[i-1].transform.position));
+			currentCarretera.Add(lastTile);
+		}
+		//Coloca una interseccion
+		inters = PlaceTile(intersections[Random.Range(0, intersections.Count)], direccionVec, lastTile.transform.position);
+		//Generar una tile mas a cada lado de la interseccion
+		tileOpt1 = PlaceTile(straights[Random.Range(0, straights.Count)], rotaVector(direccionVec, Sentido.Izquierda), inters.transform.position);
 
-    void initMovement(Sentido dir)
-    {
-        Destroy(oldInters);
-        oldInters = inters;
-        switch (dir)
-        {
-            case Sentido.Recto:
-                Debug.Log("Te moristes");
-                break;
-            case Sentido.Izquierda:
-                player.transform.Rotate(new Vector3(0, -90));
-                Destroy(tileOpt2);
-                //Clean carretera
-                cleanCarretera();
-                GeneraTramo(player.transform.forward, tileOpt1.transform.position);
-                break;
-            case Sentido.Derecha:
-                player.transform.Rotate(new Vector3(0, 90));
-                Destroy(tileOpt1);
-                //Clean carretera
-                cleanCarretera();
-                GeneraTramo(player.transform.forward, tileOpt2.transform.position);
-                break;
-            
-        }
-        playerNextDir = Sentido.Recto;
-    }
+		tileOpt2 = PlaceTile(straights[Random.Range(0, straights.Count)], rotaVector(direccionVec, Sentido.Derecha), inters.transform.position);
+	}
 
-    void Start()
-    {
-        currentCarretera = new List<GameObject>();
-        facingVec = player.transform.forward;
-        GeneraTramo(facingVec, new Vector3(0, 0, 0));
-    }
+	void cleanCarretera()
+	{
+		foreach (GameObject item in currentCarretera)
+		{
+			Destroy(item);
+		}
+		currentCarretera.Clear();
+	}
 
-    private void FixedUpdate()
-    {
-        player.transform.position += player.transform.forward * playerSpeed;
-    }
+	void initMovement(Sentido dir)
+	{
+		Destroy(oldInters);
+		oldInters = inters;
+		switch (dir)
+		{
+			case Sentido.Recto:
+				Debug.Log("Te moristes");
+				break;
+			case Sentido.Izquierda:
+				playerColHandler.logicFRotate(-90);
+				LeanTween.rotateAroundLocal(player, new Vector3(0, 1, 0), -90, 1f);
+				Destroy(tileOpt2);
+				//Clean carretera
+				cleanCarretera();
+				GeneraTramo(playerColHandler.getLogicF(), tileOpt1.transform.position);
+				break;
+			case Sentido.Derecha:
+				playerColHandler.logicFRotate(90);
+				LeanTween.rotateAroundLocal(player, new Vector3(0, 1, 0), 90, 1f);
 
-    //Para ser llamado por el jugador cuando entre a una interseccion
-    public void enteringIntersection()
-    {
-        Debug.Log("enteringIntersection");
-        initMovement(playerNextDir);
-    }
+				Destroy(tileOpt1);
+				//Clean carretera
+				cleanCarretera();
+				GeneraTramo(playerColHandler.getLogicF(), tileOpt2.transform.position);
+				break;
 
-    public void playerTurn(string direction)
-    {
-        if(direction == "Derecha")
-        {
-            playerNextDir = Sentido.Derecha;
-        }else if(direction == "Izquierda")
-        {
-            playerNextDir = Sentido.Izquierda;
-        }
-    }
+		}
+		playerNextDir = Sentido.Recto;
+	}
+
+	void Start()
+	{
+		currentCarretera = new List<GameObject>();
+		facingVec = playerColHandler.getLogicF();
+		GeneraTramo(facingVec, new Vector3(0, 0, 0));
+	}
+
+	private void FixedUpdate()
+	{
+		float p = 1.0f;
+		if (Input.GetKey(KeyCode.Space))
+		{
+			p = 5.0f;
+			ps.enableEmission = true;
+		}
+		else ps.enableEmission = true;
+
+		if (Input.GetKeyDown(KeyCode.J)) LeanTween.rotateAroundLocal(player, new Vector3(0, 1, 0), 90, 2f);
+
+		player.transform.position += playerColHandler.getLogicF() * playerSpeed * p;
+	}
+
+	//Para ser llamado por el jugador cuando entre a una interseccion
+	public void enteringIntersection()
+	{
+		Debug.Log("enteringIntersection");
+		if (Input.GetKey(KeyCode.RightArrow))
+		{
+			playerNextDir = Sentido.Derecha;
+		}
+		else if (Input.GetKey(KeyCode.LeftArrow))
+		{
+			playerNextDir = Sentido.Izquierda;
+		}
+		initMovement(playerNextDir);
+	}
+
+	public void playerTurn(string direction)
+	{
+		if (direction == "Derecha")
+		{
+			playerNextDir = Sentido.Derecha;
+		}
+		else if (direction == "Izquierda")
+		{
+			playerNextDir = Sentido.Izquierda;
+		}
+	}
 }
--- PACMA/Assets/Scripts/PlayerCollisionHandler.cs
+++ PACMA/Assets/Scripts/PlayerCollisionHandler.cs
@@ -5,21 +5,35 @@ using UnityEngine;
 public class PlayerCollisionHandler : MonoBehaviour
 {
     public CityGenerator cityManager;
+
+    private Vector3 logicForward;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        logicForward = transform.forward;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.GetComponent<Intersection>())
             cityManager.enteringIntersection();
+    }
+
+    public Vector3 getLogicF() {
+        return logicForward;
+    }
+
+    public void setLogicF(Vector3 t) {
+        logicForward = t;
+    }
+
+    public void logicFRotate(float angle ) {
+        logicForward = Quaternion.AngleAxis(angle, transform.up) * logicForward;
     }
 }
