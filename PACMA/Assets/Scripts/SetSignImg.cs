f2a551fad81ed6ef44a49b3b42f8f6f92147272a
Jorge Moreno Martínez <jorgemomartin@gmail.com>
2021-04-17 16:01:29 +0200

    Intento de leer el asset automáticamente

--- PACMA/Assets/Scripts/SetSignImg.cs
+++ PACMA/Assets/Scripts/SetSignImg.cs
@@ -6,32 +6,56 @@ using UnityEditor;
 
 public class SetSignImg : MonoBehaviour
 {
-    [System.Serializable]
-    public struct Sign
-    {
-        public float weight;
-        public Texture image;
-    }
-
     [SerializeField]
-    public Sign[] signs;
-    private RawImage img;
+    private string signsPath = "Assets/Sprites/Carteles/signals.png";
 
-    enum DirHospital { Left, Right, Front };
-    
+    private List<Texture> textures;
+    private RawImage img;
+    private int act = 0;
+
     void Start()
     {
+        textures = new List<Texture>();
+        RellenaArrayTexturas();
         img = GetComponent<RawImage>();
     }
 
-    private void Awake()
+    private void Update()
     {
+// ---------------------------------------- ESTO NO DEBE ESTAR AQUÍ -------------------------------------------------------------------------------------------------------------------
+        if (textures.Count == 0)
+        {
+            textures = new List<Texture>();
+            RellenaArrayTexturas();
+            img = GetComponent<RawImage>();
+        }
+// ---------------------------------------- ESTO NO DEBE ESTAR AQUÍ -------------------------------------------------------------------------------------------------------------------
 
+        if (textures.Count > 0)
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                act = ++act % textures.Count;
+                img.texture = textures[act];
+            }
     }
 
-    // Update is called once per frame
-    void Update()
+    void RellenaArrayTexturas() {
+        Sprite[] sprites = Resources.LoadAll<Sprite>(signsPath);
+        Debug.Log(sprites.Length);
+        foreach (Sprite s in sprites) {
+            textures.Add(SpriteToTexture(s));
+        }
+    }
+
+    static Texture2D SpriteToTexture(Sprite sprite)
     {
-        
+        Texture2D croppedTexture = new Texture2D((int)sprite.rect.width, (int)sprite.rect.height);
+        Color[] pixels = sprite.texture.GetPixels((int)sprite.textureRect.x,
+                                                (int)sprite.textureRect.y,
+                                                (int)sprite.textureRect.width,
+                                                (int)sprite.textureRect.height);
+        croppedTexture.SetPixels(pixels);
+        croppedTexture.Apply();
+        return croppedTexture;
     }
 }
