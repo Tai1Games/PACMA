96f20305d152775e4f7f23ff412af84a6947f545
Pj <47722275+Pj6595@users.noreply.github.com>
2021-04-17 13:29:34 +0200

    El conductor habla (#40)
    * Ya habla el conductor
    
    Probad a jugar en la escena BocadillosConductor y flipad con el feedback visual
    
    * Se me había olvidado meter el canvas de los bocadillos jajasi
    
    * El bocadillo desaparece pero voy a esperar a que se mergeen más cosas para implementarlo
    
    * Bocadillos finished
    
    * El bocadillo ya no entorpece la visión

---
+++ PACMA/Assets/Scripts/BocadilloConductor.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public enum Bocadillos { izquierda, derecha, recto, confuso }
+
+public class BocadilloConductor : MonoBehaviour
+{
+    //private Animator animator;
+    private Animation anim;
+    private Image bocadillo;
+    public List<Image> imagenesBocadillo;
+
+    private void Awake()
+    {
+        //animator = GetComponent<Animator>();
+        anim = GetComponent<Animation>();
+        bocadillo = GetComponent<Image>();
+        disableBocadillo();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.B)) hideBocadillo();
+    }
+
+    public void showBocadillo(Bocadillos estado)
+    {
+        disableBocadillo();
+        switch (estado)
+        {
+            case Bocadillos.izquierda:
+                imagenesBocadillo[0].enabled = true;
+                break;
+            case Bocadillos.derecha:
+                imagenesBocadillo[1].enabled = true;
+                break;
+            case Bocadillos.recto:
+                imagenesBocadillo[2].enabled = true;
+                break;
+            case Bocadillos.confuso:
+                imagenesBocadillo[3].enabled = true;
+                break;
+        }
+        bocadillo.enabled = true;
+        anim.Play("BocadilloConductor");
+    }
+
+    public void hideBocadillo()
+    {
+        anim.Play("BocadilloConductorHide");
+    }
+
+    void disableBocadillo()
+    {
+        foreach (Image im in imagenesBocadillo) im.enabled = false;
+        bocadillo.enabled = false;
+    }
+}
