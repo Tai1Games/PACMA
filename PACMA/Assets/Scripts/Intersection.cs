24a7ef846b9725b64d1c0b369eafc744e81f55bb
AdriAlvarez00 <adralv01@ucm.es>
2021-04-18 02:26:28 +0200

    Arreglada aleatoriedad interseccion
    xd

--- PACMA/Assets/Scripts/Intersection.cs
+++ PACMA/Assets/Scripts/Intersection.cs
@@ -6,12 +6,19 @@ public class Intersection : MonoBehaviour
 {
     public List<Utility.Sentido> salidas;
     private Utility.Sentido correcta;
+    private bool chosen = false;
     void Start()
     {
         correcta = salidas[Random.Range(0, salidas.Count)];
+        chosen = true;
     }
     public Utility.Sentido getCorrect()
     {
+        if (!chosen)
+        {
+            correcta = salidas[Random.Range(0, salidas.Count)];
+            chosen = true;
+        }
         return correcta;
     }
 }
